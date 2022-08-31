8f85e6f05c27fd2a56c0ae11911f4b4dfba77aa6
Tomoyuki Shimizu <smz2develop@gmail.com>
2022-08-31 10:24:36 +0900

    hammerspoon: update finder toggle

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,16 +1,28 @@
-ctrlDoublePress = require("ctrlDoublePress")
-
-ctrlDoublePress.action = function()
+-- Toggle showing app
+toggleApp = function(appName)
     local focusedScreen = hs.mouse.getCurrentScreen()
     local focusedSpace = hs.spaces.activeSpaceOnScreen(focusedScreen)
-    local chromeFilter = hs.window.filter.new(false):setAppFilter("Google Chrome", true)
-    local chromeWindow = chromeFilter:getWindows(hs.window.filter.sortByCreated)[1]
+    local filter = hs.window.filter.new(false):setAppFilter(appName)
+    local windows = filter:getWindows()
+    local window = windows[1]
+    print(hs.inspect.inspect(windows))
 
-    if (hs.window.focusedWindow() == chromeWindow) then
-        chromeWindow:sendToBack()
+    if (hs.window.focusedWindow() == window) then
+        window:sendToBack()
     else
-        hs.spaces.moveWindowToSpace(chromeWindow, focusedSpace)
-        chromeWindow:centerOnScreen(focusedScreen)
-        chromeWindow:focus()
+        hs.spaces.moveWindowToSpace(window, focusedSpace)
+        window:centerOnScreen(focusedScreen)
+        window:focus()
     end
 end
+
+-- Toggle Google Chrome in your focused screen (double tap ctrl)
+ctrlDoublePress = require("ctrlDoublePress")
+ctrlDoublePress.action = function()
+    toggleApp("Google Chrome")
+end
+
+-- Toggle Finder in your focused screen (ctrl + f)
+hs.hotkey.bind({"ctrl"}, "F", function()
+    toggleApp("Finder")
+end)
