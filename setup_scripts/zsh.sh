774cc9bed50b508ffc33d74e69e4006fc45badec
Tomoyuki Shimizu <smz2develop@gmail.com>
2022-11-13 18:12:33 +0900

    setup_script: fix zsh setup procedure

--- setup_scripts/zsh.sh
+++ setup_scripts/zsh.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-if [ -z "$(command -v zsh)" ]; then
-  if [ $OSTYPE=linux* ]; then
+if [ $OSTYPE=linux* ]; then
+  if [ -z "$(command -v zsh)" ]; then
     echo "--- apt install requirements ---"
     sudo apt update
     sudo apt install zsh build-essential procps curl file git
     echo "--- done ---"
-    echo "--- switch zsh ---"
-    which zsh | sudo tee -a /etc/shells
-    sudo chsh -s $(which zsh)
-    echo "--- done ---"
   fi
+
+  echo "--- switch zsh ---"
+  which zsh | sudo tee -a /etc/shells
+  sudo chsh -s /usr/bin/zsh
+  echo "--- done ---"
 fi
