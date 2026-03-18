53ccff5e7cc86822b418a368877e8caed8aee876
Tomoyuki Shimizu <smz2develop@gmail.com>
2026-03-18 20:51:48 +0900

    claude: add output-styles

--- setup_scripts/ai-agent.sh
+++ setup_scripts/ai-agent.sh
@@ -5,4 +5,5 @@ if command -v claude > /dev/null 2>&1; then
   ln -sf $HOME/dotfiles/ai-agents/MASTER-AGENTS.md $HOME/.claude/CLAUDE.md
   ln -sf $HOME/dotfiles/ai-agents/claude/settings.json $HOME/.claude/settings.json
   ln -sf $HOME/dotfiles/ai-agents/claude/skills $HOME/.claude/
+  ln -sf $HOME/dotfiles/ai-agents/claude/output-styles $HOME/.claude/
 fi
