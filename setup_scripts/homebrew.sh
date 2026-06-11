32eb9579d51525a935ccc96baf12176ca5aaac66
Tomoyuki Shimizu <smz2develop@gmail.com>
2026-06-11 17:48:37 +0900

    setup: homebrew

--- setup_scripts/homebrew.sh
+++ setup_scripts/homebrew.sh
@@ -6,19 +6,22 @@ OS=$(uname -s)
 if [ -z "$(command -v brew)" ]; then
   echo "--- install homebrew ---"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  case "$OS" in
-    Linux)
-      echo "--- install for linux---"
-      test -d "$HOME/.linuxbrew" && eval "$("$HOME"/.linuxbrew/bin/brew shellenv)"
-      test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-      brew bundle --file "$HOME/dotfiles/Brewfile.filtered"
-      ;;
-    Darwin)
-      echo "--- install for macOS---"
-      test -e /usr/local/bin/brew && eval "$(/usr/local/bin/brew shellenv)"
-      test -e /opt/homebrew/bin/brew && eval "$(/opt/homebrew/bin/brew shellenv)"
-      sudo xcodebuild -license accept
-      brew bundle --file "$HOME/dotfiles/Brewfile"
-      ;;
-  esac
 fi
+
+case "$OS" in
+  Linux)
+    echo "--- install for linux ---"
+    test -d "$HOME/.linuxbrew" && eval "$("$HOME"/.linuxbrew/bin/brew shellenv)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    brew bundle --file "$HOME/dotfiles/Brewfile.filtered"
+    ;;
+  Darwin)
+    echo "--- install for macOS ---"
+    test -e /usr/local/bin/brew && eval "$(/usr/local/bin/brew shellenv)"
+    test -e /opt/homebrew/bin/brew && eval "$(/opt/homebrew/bin/brew shellenv)"
+    if [ -d /Applications/Xcode.app ]; then
+      sudo env DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer xcodebuild -license accept
+    fi
+    brew bundle --file "$HOME/dotfiles/Brewfile"
+    ;;
+esac
