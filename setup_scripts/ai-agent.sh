1fd04d1c538bee58db1ce60b9b295f6149c64deb
Tomoyuki Shimizu <smz2develop@gmail.com>
2026-06-18 17:59:44 +0900

    claude: set keybindings

--- setup_scripts/ai-agent.sh
+++ setup_scripts/ai-agent.sh
@@ -7,4 +7,5 @@ if command -v claude > /dev/null 2>&1; then
   ln -sf $HOME/dotfiles/ai-agents/claude/skills $HOME/.claude/
   ln -sf $HOME/dotfiles/ai-agents/claude/output-styles $HOME/.claude/
   ln -sf $HOME/dotfiles/ai-agents/claude/statusline.sh $HOME/.claude/statusline.sh
+  ln -sf $HOME/dotfiles/ai-agents/claude/keybindings.json $HOME/.claude/keybindings.json
 fi
